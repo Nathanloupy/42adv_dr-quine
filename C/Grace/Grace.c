f22c4f9dc05d101f63392d2699e8fdd05605d0b2
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-06-19 11:14:26 +0200

    feat: Grace in C done

---
+++ C/Grace/Grace.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#define STR "#include <stdio.h>%c#define STR %c%s%c%c#define FT() int main(){fprintf(fopen(%cGrace_kid.c%c, %cw%c), STR, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 47, 47, 10);}%cFT()%c%c%ccmt%c"
+#define FT() int main(){fprintf(fopen("Grace_kid.c", "w"), STR, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 47, 47, 10);}
+FT()
+//cmt
