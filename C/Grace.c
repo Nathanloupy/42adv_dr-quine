ea221e9f26bd4cc3cdcb395976419f232ada02c2
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-06-19 12:50:16 +0200

    fix: preventing segfault

--- C/Grace.c
+++ C/Grace.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#define STR "#include <stdio.h>%c#define STR %c%s%c%c#define FT() int main(){fprintf(fopen(%cGrace_kid.c%c, %cw%c), STR, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10);}%cFT()%c/*%c%ccmt%c*/%c"
-#define FT() int main(){fprintf(fopen("Grace_kid.c", "w"), STR, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10);}
+#include <fcntl.h>
+#include <unistd.h>
+#define STR "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#define STR %c%s%c%c#define FT() int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw%c); if (!file) {return 1;} fprintf(file, STR, 10, 10, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10); fclose(file); return 0;}%cFT()%c/*%c%ccmt%c*/%c"
+#define FT() int main(){FILE *file = fopen("Grace_kid.c", "w"); if (!file) {return 1;} fprintf(file, STR, 10, 10, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10); fclose(file); return 0;}
 FT()
 /*
 	cmt
