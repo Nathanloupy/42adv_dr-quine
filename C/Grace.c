fc5eed1c14ce372c7e4616e7efd092385cffb9d9
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-06-20 15:19:04 +0200

    fix: removing unnecessary headers

--- C/Grace.c
+++ C/Grace.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#define STR "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#define STR %c%s%c%c#define FT() int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw%c); if (!file) {return 1;} fprintf(file, STR, 10, 10, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10); fclose(file); return 0;}%cFT()%c/*%c%ccmt%c*/%c"
-#define FT() int main(){FILE *file = fopen("Grace_kid.c", "w"); if (!file) {return 1;} fprintf(file, STR, 10, 10, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10); fclose(file); return 0;}
+#define STR "#include <stdio.h>%c#define STR %c%s%c%c#define FT() int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw%c); if (!file) {return 1;} fprintf(file, STR, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10); fclose(file); return 0;}%cFT()%c/*%c%ccmt%c*/%c"
+#define FT() int main(){FILE *file = fopen("Grace_kid.c", "w"); if (!file) {return 1;} fprintf(file, STR, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10); fclose(file); return 0;}
 FT()
 /*
 	cmt
