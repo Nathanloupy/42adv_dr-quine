93f08925f5d39381fa123cb094f432663ffbc789
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-06-30 10:06:48 +0200

    fix: stack alignment for Colleen.s and Grace.s

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,7 +1,8 @@
 extern printf
+extern exit
 ; cmt1
 section .data
-	format_str db "extern printf%1$c%4$c cmt1%1$csection .data%1$c%2$cformat_str db %3$c%5$s%3$c,0%1$c%2$cnl db 10%1$c%2$ctab db 9%1$c%2$cquote db 34%1$c%2$csc db 59%1$csection .text%1$c%2$cglobal _start%1$c_start:%1$c%2$center 0, 0%1$c%2$cand rsp, -16%1$c%2$c%4$c cmt2%1$c%2$cmov rdi, format_str%1$c%2$cmovzx rsi, byte [nl]%1$c%2$cmovzx rdx, byte [tab]%1$c%2$cmovzx rcx, byte [quote]%1$c%2$cmovzx r8, byte [sc]%1$c%2$cmov r9, format_str%1$c%2$cxor rax, rax%1$c%2$ccall printf%1$c%2$cmov rax, 60%1$c%2$cmov rdi, 0%1$c%2$csyscall%1$c",0
+	format_str db "extern printf%1$cextern exit%1$c%4$c cmt1%1$csection .data%1$c%2$cformat_str db %3$c%5$s%3$c,0%1$c%2$cnl db 10%1$c%2$ctab db 9%1$c%2$cquote db 34%1$c%2$csc db 59%1$csection .text%1$c%2$cglobal _start%1$c_start:%1$c%2$center 0, 0%1$c%2$cand rsp, -16%1$c%2$c%4$c cmt2%1$c%2$cmov rdi, format_str%1$c%2$cmovzx rsi, byte [nl]%1$c%2$cmovzx rdx, byte [tab]%1$c%2$cmovzx rcx, byte [quote]%1$c%2$cmovzx r8, byte [sc]%1$c%2$cmov r9, format_str%1$c%2$cxor rax, rax%1$c%2$ccall printf%1$c%2$cmov rdi, 0%1$c%2$ccall exit%1$c",0
 	nl db 10
 	tab db 9
 	quote db 34
@@ -20,5 +21,5 @@ _start:
 	mov r9, format_str
 	xor rax, rax
 	call printf
-	mov rdi, 99
-	leave
+	mov rdi, 0
+	call exit
--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,13 +1,16 @@
 extern dprintf
 extern fflush
+extern exit
 %macro FUNCS 0
-exit_func:
-	mov rax, 60
-	syscall
 error_exit:
 	mov rdi, 1
-	jmp exit_func
+	call exit
+normal_exit:
+	mov rdi, 0
+	call exit
 _start:
+	enter 0, 0
+	and rsp, -16
 	mov rax, 2
 	mov rdi, file_str
 	mov rsi, 577
@@ -22,26 +25,23 @@ _start:
 	movzx rcx, byte [tab]
 	movzx r8, byte [quote]
 	movzx r9, byte [sc]
+	sub rsp, 8
 	push format_str
 	mov rax, 0
-	sub rsp, 16
 	call dprintf
 	add rsp, 16
 	mov rdi, 0
 	xor rax, rax
-	sub rsp, 16
 	call fflush
-	add rsp, 16
 	mov rax, 3
 	mov rdi, rbx
 	syscall
-	mov rdi, 0
-	jmp exit_func
+	jmp normal_exit
 %endmacro
 ; cmt1
 section .data
 	file_str db "Grace_kid.s",0
-	format_str db "extern dprintf%1$cextern fflush%1$c%%macro FUNCS 0%1$cexit_func:%1$c%2$cmov rax, 60%1$c%2$csyscall%1$c_start:%1$c%2$cand rsp, -16%1$c%2$cmov rax, 2%1$c%2$cmov rdi, file_str%1$c%2$cmov rsi, 577%1$c%2$cmov rdx, 420%1$c%2$csyscall%1$c%2$ccmp rax, 0%1$c%2$cjl error_exit%1$c%2$cmov rbx, rax%1$c%2$cmov rdi, rbx%1$c%2$cmov rsi, format_str%1$c%2$cmovzx rdx, byte [nl]%1$c%2$cmovzx rcx, byte [tab]%1$c%2$cmovzx r8, byte [quote]%1$c%2$cmovzx r9, byte [sc]%1$c%2$csub rsp, 8%1$c%2$cpush format_str%1$c%2$cmov rax, 0%1$c%2$ccall dprintf%1$c%2$cadd rsp, 16%1$c%2$cmov rdi, 0%1$c%2$cxor rax, rax%1$c%2$ccall fflush%1$c%2$cmov rax, 3%1$c%2$cmov rdi, rbx%1$c%2$csyscall%1$c%2$cmov rdi, 0%1$c%2$ccall exit_func%1$cerror_exit:%1$c%2$cmov rdi, 1%1$c%2$ccall exit_func%1$c%%endmacro%1$c%4$c cmt1%1$csection .data%1$c%2$cfile_str db %3$cGrace_kid.s%3$c,0%1$c%2$cformat_str db %3$c%5$s%3$c,0%1$c%2$cnl db 10%1$c%2$ctab db 9%1$c%2$cquote db 34%1$c%2$csc db 59%1$csection .text%1$c%2$cglobal _start%1$cFUNCS%1$c",0
+	format_str db "extern dprintf%1$cextern fflush%1$cextern exit%1$c%%macro FUNCS 0%1$cerror_exit:%1$c%2$cmov rdi, 1%1$c%2$ccall exit%1$cnormal_exit:%1$c%2$cmov rdi, 0%1$c%2$ccall exit%1$c_start:%1$c%2$center 0, 0%1$c%2$cand rsp, -16%1$c%2$cmov rax, 2%1$c%2$cmov rdi, file_str%1$c%2$cmov rsi, 577%1$c%2$cmov rdx, 420%1$c%2$csyscall%1$c%2$ccmp rax, 0%1$c%2$cjl error_exit%1$c%2$cmov rbx, rax%1$c%2$cmov rdi, rbx%1$c%2$cmov rsi, format_str%1$c%2$cmovzx rdx, byte [nl]%1$c%2$cmovzx rcx, byte [tab]%1$c%2$cmovzx r8, byte [quote]%1$c%2$cmovzx r9, byte [sc]%1$c%2$csub rsp, 8%1$c%2$cpush format_str%1$c%2$cmov rax, 0%1$c%2$ccall dprintf%1$c%2$cadd rsp, 16%1$c%2$cmov rdi, 0%1$c%2$cxor rax, rax%1$c%2$ccall fflush%1$c%2$cmov rax, 3%1$c%2$cmov rdi, rbx%1$c%2$csyscall%1$c%2$cjmp normal_exit%1$c%%endmacro%1$c%4$c cmt1%1$csection .data%1$c%2$cfile_str db %3$cGrace_kid.s%3$c,0%1$c%2$cformat_str db %3$c%5$s%3$c,0%1$c%2$cnl db 10%1$c%2$ctab db 9%1$c%2$cquote db 34%1$c%2$csc db 59%1$csection .text%1$c%2$cglobal _start%1$cFUNCS%1$c",0
 	nl db 10
 	tab db 9
 	quote db 34
