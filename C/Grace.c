7db68683af4de11c2d6f53d85beb771cc49801ae
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-06-30 13:16:48 +0200

    fix: complying with evaluation requirements

--- C/Grace.c
+++ C/Grace.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#define STR "#include <stdio.h>%c#define STR %c%s%c%c#define FT() int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw%c); if (!file) {return 1;} fprintf(file, STR, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10); fclose(file); return 0;}%cFT()%c/*%c%ccmt%c*/%c"
-#define FT() int main(){FILE *file = fopen("Grace_kid.c", "w"); if (!file) {return 1;} fprintf(file, STR, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10, 10, 9, 10, 10); fclose(file); return 0;}
+#define FILENAME "Grace_kid.c"
+#define STR "#include <stdio.h>%1$c#define FILENAME %2$c%3$s%2$c%1$c#define STR %2$c%4$s%2$c%1$c#define FT() int main(){FILE *file = fopen(FILENAME, %2$cw%2$c); if (!file) {return 1;} fprintf(file, STR, 10, 34, FILENAME, STR, 9); fclose(file); return 0;}%1$cFT()%1$c/*%1$c%5$ccmt%1$c*/%1$c"
+#define FT() int main(){FILE *file = fopen(FILENAME, "w"); if (!file) {return 1;} fprintf(file, STR, 10, 34, FILENAME, STR, 9); fclose(file); return 0;}
 FT()
 /*
 	cmt
